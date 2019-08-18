a69de9885611db174eed8886132bd8c7d6b9f845
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-18 23:26:43 +0530

    Add App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
- import React from 'react';
- import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
+import './App.css';
 
- const App = () =>{
-   return(
-     <>
-     <h1>Digital Journal</h1>
-     </>
-   )
- }
+const App = () => (
+  <Router>
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </div>
+  </Router>
+);
 
- export default App;
+export default App;

b06982b711825b2a746ca2ab512d146c2d2c53d8
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-21 23:23:11 +0530

    Fix signup form state update issue

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,30 +1,50 @@
 import React, { useState } from 'react';
 import InputField from '../InputField/InputField';
 import Layout from '../Layout/Layout';
+import './Signup.css';
 
 const Signup = () => {
   const [state, setState] = useState({ firstname: '', lastname: '', email: '', password: '', confPassword: '' });
-  const handleChange = event => {
-    setState({ [event.target.name]: event.target.value });
-  };
-  const handleSubmit = () => {};
 
-  const { firstname, lastname, email, password, confPassword } = state;
+  const handleChange = event => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setState({ fullname: '', email: '', password: '', confPassword: '' });
+  };
+
+  const { fullname, email, password, confPassword } = state;
   return (
     <Layout>
       <div className="signup">
         <h2>Register Now</h2>
         <form onSubmit={handleSubmit}>
-          <InputField type="text" value={firstname} placeholder="Firstname" name="firstname" onChange={handleChange} />
-          <InputField type="text" value={lastname} placeholder="Lastname" name="lastname" onChange={handleChange} />
-          <InputField type="email" value={email} placeholder="Email" name="email" onChange={handleChange} />
-          <InputField type="password" value={password} placeholder="Password" name="passwrod" onChange={handleChange} />
+          <InputField type="text" value={fullname} placeholder="Fullname" name="fullname" handleChange={handleChange} />
+          <InputField
+            type="email"
+            value={email}
+            placeholder="Email"
+            name="email"
+            handleChange={handleChange}
+            required
+          />
+          <InputField
+            type="password"
+            value={password}
+            placeholder="Password"
+            name="password"
+            handleChange={handleChange}
+            required
+          />
           <InputField
             type="password"
             value={confPassword}
             placeholder="Re type password"
             name="confPassword"
-            onChange={handleChange}
+            handleChange={handleChange}
+            required
           />
           <InputField type="submit" value="Register" name="register" styles="btn" />
         </form>

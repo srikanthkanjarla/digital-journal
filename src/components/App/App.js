3f0214cedae7f744fb9f53ca6138bd6a9d26d70e
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-27 23:26:20 +0530

    Add firebase authentication

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from '../../firebase';
+import AuthContext from '../Context';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
+import AddNote from '../Notes/AddNoteForm';
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div className="app">
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    firebase.auth.onAuthStateChanged(user => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    });
+  }, []);
+  return (
+    <AuthContext.Provider value={isAuthenticated}>
+      <Router>
+        <div className="app">
+          <Header authenticated={isAuthenticated} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/add-new-note" component={AddNote} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;

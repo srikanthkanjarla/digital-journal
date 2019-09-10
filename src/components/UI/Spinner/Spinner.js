27160dfb7159764f0acdbce30e1f36ad97011606
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-10 20:37:32 +0530

    Update spinner with size and theme   props

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Spinner.css';
 
-const Spinner = () => {
-  return (
-    <div className="loading-spinner">
-      <p>Loading...</p>
-    </div>
-  );
+const Spinner = props => {
+  const { size, theme } = props;
+
+  return <div className={`loading-spinner ${theme}`} style={{ height: `${size * 10}px`, width: `${size * 10}px` }} />;
 };
 
+Spinner.propTypes = {
+  size: PropTypes.number.isRequired,
+  theme: PropTypes.string.isRequired,
+};
 export default Spinner;

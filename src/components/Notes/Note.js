a5a03a2aac9fa884bc33cb09ebca9d37af540d7a
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-10 20:35:27 +0530

    Replace input with Button componet

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import Button from '../UI/Button/Button';
 import './Note.css';
 
 const Note = props => {
-  const { id, title, body, handleUpdateNote, handleDeleteNote, color } = props;
+  const { id, title, body, color, handleUpdateNote, handleDeleteNote } = props;
   return (
     <div className="note-card" style={{ backgroundColor: color }}>
+      <div className="btn-group">
+        <Button btnClass="btn btn-sm btn-edit" handleClick={() => handleUpdateNote(id)}>
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </Button>
+
+        {/* <button type="button" title="Edit Note" className="edit-btn" onClick={() => handleUpdateNote(id)}>
+            <FontAwesomeIcon icon={faPencilAlt} />
+          </button> */}
+
+        <Button btnClass="btn btn-sm btn-close" handleClick={() => handleDeleteNote(id)}>
+          <FontAwesomeIcon icon={faTimes} />
+        </Button>
+        {/* <button type="button" title="Remove Note" className="del-btn" onClick={() => handleDeleteNote(id)}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button> */}
+      </div>
       <h2>{title}</h2>
       <p>{body}</p>
-      <div className="note-btn">
-        <span>
-          <input type="button" value="&#9998;" className="edit-btn" onClick={() => handleUpdateNote(id)} />
-        </span>
-        <span>
-          <input type="button" value="&times;" className="del-btn" onClick={() => handleDeleteNote(id)} />
-        </span>
-      </div>
     </div>
   );
 };
@@ -24,6 +35,7 @@ Note.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
   handleDeleteNote: PropTypes.func.isRequired,
   handleUpdateNote: PropTypes.func.isRequired,
 };

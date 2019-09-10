01540e917bdffa8a23180f4a0c4315caefd0536b
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-10 20:27:03 +0530

    Remove commented code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import firebase from '../../firebase';
-import AuthContext from '../Context';
+import AuthContext from '../AuthContext';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
-// import LoadingSpinner from '../UI/Spinner/Spinner';
 import './App.css';
 
 const App = () => {
   const [isAuthenticated, setAuthenticated] = useState(false);
-  // const [isInitialized, setInitialized] = useState(false);
-
-  // firebase.isFirebaseInitialized().then(val => {
-  //   if (val) {
-  //     setInitialized(true);
-  //   }
-  // });
-
   useEffect(() => {
     firebase.auth.onAuthStateChanged(user => {
       if (user) {

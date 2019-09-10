55ccf34b221ac0ce1d04988ab6b8873a353f0c0f
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-10 20:36:35 +0530

    Refactor Add note form

--- src/components/UI/AddNoteForm/AddNoteForm.js
+++ src/components/UI/AddNoteForm/AddNoteForm.js
@@ -1,54 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import firebase from '../../../firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import Input from '../Input/Input';
 import Label from '../Label/Label';
 import Button from '../Button/Button';
 import './AddNoteForm.css';
+import Spinner from '../Spinner/Spinner';
 
 const AddNoteForm = props => {
-  const [state, setState] = useState({ id: '', title: '', body: '' });
-  const { title, body } = state;
-  const { data, closeForm, formAction } = props;
+  const { title, body, processing, closeForm, formAction, handleSaveOrUpdate, handleChange } = props;
 
-  useEffect(() => {
-    if (formAction === 'update') {
-      setState(data);
-    }
-  }, [data]);
-
-  const handleChange = event => {
-    setState({ ...state, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = event => {
-    // save note to firebase
-    event.preventDefault();
-    if (title !== '' && body !== '') {
-      if (formAction === 'addNew') {
-        try {
-          firebase.addNote(state);
-          closeForm();
-        } catch (error) {
-          console.log('Error - ', error);
-        }
-      }
-
-      if (formAction === 'update') {
-        try {
-          firebase.updateNote(state);
-          closeForm();
-        } catch (error) {
-          console.log('Error - ', error);
-        }
-      }
-    }
-  };
-
-  return  (
+  return (
     <div className="form-container">
       <div className="new-note-form">
-        <form onSubmit={handleSubmit}>
+        <form>
           <h2>{formAction === 'update' ? 'Update Note' : 'Add New Note'}</h2>
           <Label htmlForId="title">
             <Input
@@ -72,10 +38,15 @@ const AddNoteForm = props => {
               required
             />
           </Label>
-          <Button text={formAction === 'update' ? 'Update Note' : 'Add New Note'} />
-          <button type="button" className="close-form" onClick={closeForm}>
-            &times;
-          </button>
+          <Button btnClass=" btn btn-lg" disabled={processing} handleClick={handleSaveOrUpdate}>
+            {formAction === 'update' ? 'Update Note' : 'Add New Note'}
+            {processing && <Spinner size={1.5} theme="dark" />}
+          </Button>
+          <div style={{ position: 'absolute', top: 0, right: 0 }}>
+            <Button btnClass="btn btn-sm btn-close" handleClick={closeForm}>
+              <FontAwesomeIcon icon={faTimes} />
+            </Button>
+          </div>
         </form>
       </div>
     </div>
@@ -83,7 +54,12 @@ const AddNoteForm = props => {
 };
 
 AddNoteForm.propTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
   formAction: PropTypes.string.isRequired,
+  processing: PropTypes.bool.isRequired,
   closeForm: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSaveOrUpdate: PropTypes.func.isRequired,
 };
 export default AddNoteForm;
